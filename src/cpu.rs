58b065069951d2775b2b7d26ce7396f09cb6a2cc
Adam <adam@appcannonsoftware.com>
2022-09-28 19:52:51 -0700

    Implemented (untested) ALU instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -26,51 +26,58 @@ impl CPU {
         let inst_word = self.pc_word();
         let [inst_prefix_byte, inst_suffix_byte] = inst_word.to_be_bytes();
 
-        let mut basic_load = |write: bool, size: DataSize| {
-            let reg = inst_suffix_byte;
-            let address = self.pc_word();
-
-            if write {
-                let value = self.get_reg(reg);
-
-                match size {
-                    DataSize::Byte => self.ram.mem_write_byte(address, value.to_le_bytes()[0]),
-                    DataSize::Word => {
-                        let bytes = value.to_le_bytes();
-
-                        self.ram
-                            .mem_write_word(address, (bytes[1] as u16) << 8 | (bytes[0] as u16))
-                    }
-                    DataSize::Long => self.ram.mem_write_long(address, value),
-                }
-            } else {
-                let value = match size {
-                    DataSize::Byte => self.ram.mem_read_byte(address).into(),
-                    DataSize::Word => self.ram.mem_read_word(address).into(),
-                    DataSize::Long => self.ram.mem_read_long(address),
-                };
-
-                self.set_reg(reg, value);
-                self.set_zero(value);
-            }
-        };
-
         match inst_prefix_byte {
-            0x0 => {}                                  // NOP
-            0x02 => basic_load(false, DataSize::Byte), // ld.b Rx,(nnnn)
-            0x03 => basic_load(true, DataSize::Byte),  // ld.b (nnnn),Rx
-            0x04 => basic_load(false, DataSize::Word), // ld.w Rx,(nnnn)
-            0x05 => basic_load(true, DataSize::Word),  // ld.w (nnnn),Rx
-            0x06 => basic_load(false, DataSize::Long), // ld.l Rx,(nnnn)
-            0x07 => basic_load(true, DataSize::Long),  // ld.l (nnnn),Rx
-            0x08 => {
-                // ld Rx,#16
+            0x0 => {}                                                                           // NOP
+            0x02 => self.basic_load_inst(inst_suffix_byte, false, DataSize::Byte), // ld.b Rx,(nnnn)
+            0x03 => self.basic_load_inst(inst_suffix_byte, true, DataSize::Byte),  // ld.b (nnnn),Rx
+            0x04 => self.basic_load_inst(inst_suffix_byte, false, DataSize::Word), // ld.w Rx,(nnnn)
+            0x05 => self.basic_load_inst(inst_suffix_byte, true, DataSize::Word),  // ld.w (nnnn),Rx
+            0x06 => self.basic_load_inst(inst_suffix_byte, false, DataSize::Long), // ld.l Rx,(nnnn)
+            0x07 => self.basic_load_inst(inst_suffix_byte, true, DataSize::Long),  // ld.l (nnnn),Rx
+            0x08 => self.basic_alu(inst_suffix_byte, false, |_, immediate| (immediate, false)), // ld Rx,#16
+            0x09 => self.basic_alu(inst_suffix_byte, false, |reg, immediate| {
+                // and Rx,#16
+                (reg & immediate, false)
+            }),
+            0x0A => self.basic_alu(inst_suffix_byte, false, |reg, immediate| {
+                // or Rx,#16
+                (reg | immediate, false)
+            }),
+            0x0B => self.basic_alu(inst_suffix_byte, false, |reg, immediate| {
+                // xor Rx,#16
+                (reg ^ immediate, false)
+            }),
+            0x0C => self.basic_alu(inst_suffix_byte, true, |reg, immediate| {
+                // add Rx,#16
+                reg.overflowing_add(immediate)
+            }),
+            0x0D => self.basic_alu(inst_suffix_byte, true, |reg, immediate| {
+                // sub Rx,#16
+                reg.overflowing_sub(immediate)
+            }),
+            0x0E => {
+                // cmp Rx,#16
                 let reg = inst_suffix_byte;
                 let immediate = self.pc_word();
 
-                self.set_reg(reg, immediate as u32);
-                self.set_zero(immediate as u32);
+                let (value, carry) = self.get_reg(reg).overflowing_sub(immediate as u32);
+
+                // Don't set value
+                self.set_carry(carry);
+                self.set_zero(value);
             }
+            0x0F => {
+                // bit Rx,#16
+                let reg = inst_suffix_byte;
+                let immediate = self.pc_word();
+
+                let value = self.get_reg(reg) & (immediate as u32);
+
+                // Don't set value
+                self.set_zero(value);
+            }
+            0x10 => todo!("RSET"),
+            0x11 => todo!("CRC"),
             0x18 => {
                 // ld Rx,#32
                 let reg = inst_suffix_byte;
@@ -83,6 +90,56 @@ impl CPU {
                 // Do nothing
             }
         }
+    }
+
+    ///
+    /// A load/store instruction. Can be `b`, `w`, or `l`
+    ///
+    fn basic_load_inst(&mut self, inst_suffix_byte: u8, write: bool, size: DataSize) {
+        let reg = inst_suffix_byte;
+        let address = self.pc_word();
+
+        if write {
+            let value = self.get_reg(reg);
+
+            match size {
+                DataSize::Byte => self.ram.mem_write_byte(address, value.to_le_bytes()[0]),
+                DataSize::Word => {
+                    let bytes = value.to_le_bytes();
+
+                    self.ram
+                        .mem_write_word(address, (bytes[1] as u16) << 8 | (bytes[0] as u16))
+                }
+                DataSize::Long => self.ram.mem_write_long(address, value),
+            }
+        } else {
+            let value = match size {
+                DataSize::Byte => self.ram.mem_read_byte(address).into(),
+                DataSize::Word => self.ram.mem_read_word(address).into(),
+                DataSize::Long => self.ram.mem_read_long(address),
+            };
+
+            self.set_reg(reg, value);
+            self.set_zero(value);
+        }
+    }
+
+    fn basic_alu<T: Fn(u32, u32) -> (u32, bool)>(
+        &mut self,
+        inst_suffix_byte: u8,
+        set_carry: bool,
+        operation: T,
+    ) {
+        let reg = inst_suffix_byte;
+        let immediate = self.pc_word();
+
+        let (value, carry) = operation(self.get_reg(reg), immediate as u32);
+
+        self.set_reg(reg, value);
+        if set_carry {
+            self.set_carry(carry);
+        }
+        self.set_zero(value);
     }
 
     fn pc_byte(&mut self) -> u8 {
@@ -111,6 +168,10 @@ impl CPU {
 
     fn set_zero(&mut self, value: u32) {
         self.zero = value == 0;
+    }
+
+    fn set_carry(&mut self, carry: bool) {
+        self.carry = carry;
     }
 
     fn set_reg(&mut self, reg: u8, value: u32) {
